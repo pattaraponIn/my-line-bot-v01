d3e27721dc9aeb642c8c024209aea7cbbc72b511
pattaraponIn <48173317+pattaraponIn@users.noreply.github.com>
2019-03-03 16:21:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,6 +32,9 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    text=event.message.text
+    if text=="s1":
+        TextSendMessage("led on")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
